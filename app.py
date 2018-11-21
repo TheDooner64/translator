e53abc02b8926590a95cd5bceda40eac58948500
TheDooner64 <rmuldoon@yipitdata.com>
2018-11-21 12:14:55 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,33 @@
+import json
+from difflib import get_close_matches
+
+
+data = json.load(open('data.json', 'r'))
+
+
+def print_definition(definition):
+    for line in definition:
+        print(line)
+
+
+def translate(word):
+    word = word.lower()
+
+    definition = data.get(word)
+
+    if definition:
+        print_definition(definition)
+    else:
+        closest_matches = get_close_matches(word, data.keys(), n=1, cutoff=0.75)
+        if len(closest_matches) > 0:
+            closest_match = closest_matches[0]
+            answer = input('{} was not found in the dictionary, did you mean {}? [y/n]'.format(word, closest_match))
+            if answer == 'y':
+                definition = data.get(closest_match)
+                print_definition(definition)
+        else:
+            print('Nothing close to {} was found in the dictionary!'.format(word))
+
+
+word = input('Enter a word: ')
+translate(word)
